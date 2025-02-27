d6d011a51b13ab35dca157dadff7524335b48198
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-02-27 17:44:37 -0300

    feat: ajuste no layout para exibição de sidebar e data table

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,13 @@
 import { PropsWithChildren } from "react";
 import { Sidebar } from "./_components/sidebar";
 
-export default function Layout({children}: PropsWithChildren) {
-
+export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="flex w-full h-screen ">
+    <div className="flex w-full h-screen bg-gray-100">
       <Sidebar />
-      <main className="flex-1">
-      {children}
+      <main className="flex-1 overflow-auto">
+        <div className="p-4 md:p-6"> {children}</div>
       </main>
     </div>
-  )
+  );
 }
