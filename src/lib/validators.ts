0da90733ee4e732e931489044d698dca02a9daea
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-02-27 18:44:03 -0300

    feat: dropdown e schema de curso

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -6,3 +6,15 @@ export const LoginSchema = z.object({
 })
 
 export type LoginProps = z.infer<typeof LoginSchema>
+
+
+export const CourseDataSchema = z.object({
+  title: z.string().min(1),
+  price: z.number().min(1),
+  rating: z.number().min(1),
+  description: z.string().min(1),
+  image: z.string().min(1),
+  category: z.string(),
+})
+
+export type CourseDataProps = z.infer<typeof CourseDataSchema>
