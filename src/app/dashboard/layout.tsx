74f22a3a915df6aaf60719a59e5a076d46561963
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-02-27 18:45:29 -0300

    feat: atualização do bg da dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,8 +5,8 @@ export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="flex w-full h-screen bg-gray-100">
       <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <div className="p-4 md:p-6"> {children}</div>
+      <main className="flex-1 overflow-auto bg-white">
+        <div className="p-6 md:p-8"> {children}</div>
       </main>
     </div>
   );
