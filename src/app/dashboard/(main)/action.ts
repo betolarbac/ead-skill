2747e03d19048724190f01f57241eb70247ae057
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-02-28 12:29:16 -0300

    refactor: SOLID para exibir todos os produtos

--- src/app/dashboard/(main)/action.ts
+++ src/app/dashboard/(main)/action.ts
@@ -1,12 +1,7 @@
-import { CourseDataProps } from "@/lib/validators"
+import { CourseDataProps } from "@/lib/validators";
+import { CourseService } from "@/service/course/CourseService";
 
-export async function GetCourse() {
-  const response = await fetch("https://fakestoreapi.com/products", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
-
-  const result:CourseDataProps[] = await response.json()
-
-  return result
+export async function GetCourse(): Promise<CourseDataProps[]> {
+  const courseService = new CourseService();
+  return await courseService.getAllCourses();
 }
