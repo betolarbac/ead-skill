02b0333214c50c2c618107e9c4ddaa394d28c6c0
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-02-28 12:29:51 -0300

    refactor: req SOLID para curso com id

--- src/app/dashboard/course/[id]/action.ts
+++ src/app/dashboard/course/[id]/action.ts
@@ -1,12 +1,7 @@
-import { CourseDataProps } from "@/lib/validators"
+
+import { CourseService } from "@/service/course/CourseService"
 
 export async function GetCourseData(id: string) {
-  const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
-
-  const result:CourseDataProps = await response.json()
-
-  return result
+  const getCourseId = new CourseService();
+  return await getCourseId.getCourseById(id);
 }
